58e21864b41c07687acca6741a7ef71ecd7d0ccd
fredapires <fredapires@gmail.com>
2023-02-08 01:09:59 -0500

    updates to scripts

--- scripts/batch/_personal_pip_install_script.bat
+++ scripts/batch/_personal_pip_install_script.bat
@@ -13,8 +13,4 @@ pip install git+https://github.com/fredapires/mtg-proj
 @REM execute main python script
 @REM python C:\git\fp_data_toolbox-proj\scripts\python\_master_script.py %*
 
-
-
-pipenv install git+https://github.com/fredapires/fp_data_toolbox#egg=fp_data_toolbox
-
 pause
