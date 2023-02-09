6883c371ed8457489b28fbeb5f675dc79873209d
fredapires <fredapires@gmail.com>
2023-02-09 09:03:46 -0500

    updates to package handling

--- fp_data_toolbox/package_handling.py
+++ fp_data_toolbox/package_handling.py
@@ -1,11 +1,15 @@
-###=============================================
-### General Package Handling
+# =============================================
+# General Package Handling
 
 
-## TODO function for pip installing from a provided github https url
+# DONE function for pip installing from a provided github https url
 def pip_install_from_github_url(github_url):
     import os
     print('Running "pip install" command using '+github_url)
     os.system('pip install git+'+github_url)
-    
-    
+
+
+def pipenv_install_from_github_url(github_url):
+    import os
+    print('Running "pip install" command using '+github_url)
+    os.system('pipenv install git+'+github_url)
