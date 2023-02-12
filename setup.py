6ef95854c818445a976a47346fd6a4598a0d1717
fredapires <fredapires@gmail.com>
2023-02-11 23:39:28 -0500

    update to modules

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='fp-data-toolbox',
-    version='0.1.26',
+    version='0.1.27',
     author='Fred Pires',
     author_email='fredapires@gmail.com',
     description='personal toolbox for data science',
