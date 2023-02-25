dc959d232ca85a031fb3037a49843b418d510e08
fredapires <fredapires@gmail.com>
2023-02-25 18:12:04 -0500

    disabling bugged function

--- fp_data_toolbox/package_handling.py
+++ fp_data_toolbox/package_handling.py
@@ -9,7 +9,8 @@ def pip_install_from_github_url(github_url):
     os.system('pip install git+'+github_url)
 
 
-def pipenv_install_from_github_url(github_url):
-    import os
-    print('Running "pip install" command using '+github_url)
-    os.system('pipenv install git+'+github_url)
+# TODO function for pipenv installing from a provided github https url
+# def pipenv_install_from_github_url(github_url):
+#     import os
+#     print('Running "pipenv install" command using '+github_url)
+#     os.system('pipenv install git+'+github_url+'egg')
