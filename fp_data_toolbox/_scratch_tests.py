c5c1d9620c6935368466a9b08f6f32383610e643
fredapires <fredapires@gmail.com>
2023-02-25 22:48:08 -0500

    fp_data_toolbox: updates to excel-to-eda

--- fp_data_toolbox/_scratch_tests.py
+++ fp_data_toolbox/_scratch_tests.py
@@ -14,13 +14,21 @@ def excel_to_eda_tools(excel_path, sheet_name, data_profile_type='ydata'):
 
     excel_parent_directory = os.path.dirname(excel_path)
 
+    print('')
+
     df = pd.DataFrame()
     df = file_handling.read_excel_to_dataframe(
         workbook_path=excel_path,
         sheet_name=sheet_name,
     )
+    print('Succesfully loaded excel data sheet to Pandas dataframe.')
+    print('')
 
     # general data cleaning/prep operations here
+    # print('Starting data cleaning operations...')
+
+    # print('Done with data cleaning operations.')
+    print('')
 
     # various profiling report output types here
     # [ ] ydata_profiling
@@ -28,12 +36,21 @@ def excel_to_eda_tools(excel_path, sheet_name, data_profile_type='ydata'):
     # [x] minimal
     # [ ] missingno matrix
     # [ ] dataprep
+
+    print('Starting to build data profiling reports.')
+    print('')
+
     if data_profile_type == 'ydata':
+        print('Starting ydata_profiling report. May take a few minutes...')
+        print('')
         from ydata_profiling import ProfileReport
         profile = ProfileReport(df, minimal=True)
 
         # save to excel_parent_directory
         profile.to_file(excel_parent_directory+'\\ydata_profiling_report.html')
+        print('')
+        print('Done building ydata_profiling report to ' +
+              excel_parent_directory + '  '+'Thanks!')
     else:
         print('data_profile_type input invalid')
 
