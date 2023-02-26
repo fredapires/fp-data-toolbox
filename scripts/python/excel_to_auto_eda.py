e9072fc1fba803d963aa239accd61cdeb1732007
fredapires <fredapires@gmail.com>
2023-02-26 10:00:50 -0500

    small updates to file structure

---
+++ scripts/python/excel_to_auto_eda.py
@@ -0,0 +1,91 @@
+# Excel -> eda script
+# %% Imports
+import os
+import sys
+import pandas as pd
+#
+import fp_data_toolbox as fpdt
+from fp_data_toolbox import eda
+from fp_data_toolbox import file_handling
+
+
+# %% define functions
+
+def excel_to_eda_tools(excel_path, sheet_name, data_profile_type='ydata'):
+
+    excel_parent_directory = os.path.dirname(excel_path)
+
+    print('')
+
+    df = pd.DataFrame()
+    df = file_handling.read_excel_to_dataframe(
+        workbook_path=excel_path,
+        sheet_name=sheet_name,
+    )
+    print('Succesfully loaded excel data sheet to Pandas dataframe.')
+    print('')
+
+    # general data cleaning/prep operations here
+    # print('Starting data cleaning operations...')
+
+    # print('Done with data cleaning operations.')
+    print('')
+
+    # various profiling report output types here
+    # [ ] ydata_profiling
+    # [ ] with config
+    # [x] minimal
+    # [ ] missingno matrix
+    # [ ] dataprep
+
+    print('Starting to build data profiling reports.')
+    print('')
+
+    if data_profile_type == 'ydata':
+        print('Starting ydata_profiling report. May take a few minutes...')
+        print('')
+        from ydata_profiling import ProfileReport
+        profile = ProfileReport(df, minimal=True)
+
+        # save to excel_parent_directory
+        profile.to_file(excel_parent_directory+'\\' +
+                        sheet_name+'-'+'ydata_report.html')
+
+        print('')
+        print('Done building ydata_profiling report to ' +
+              excel_parent_directory + '  '+'Thanks!')
+    if data_profile_type == 'dataprep':
+        print('Starting dataprep report. May take a few minutes...')
+        print('')
+        from dataprep.eda import plot, plot_correlation, plot_missing
+        from dataprep.eda import create_report
+        report = create_report(df)
+        report  # show report in notebook
+        # save report to local disk
+        report.save(excel_parent_directory+'\\' +
+                    sheet_name+'-'+'dataprep_report.html')
+        report.show_browser()  # show report in the browser
+
+        print('')
+        print('Done building dataprep report to ' +
+              excel_parent_directory + '  '+'Thanks!')
+    else:
+        print('Done building all data profiling reports')
+
+
+# %%
+excel_path_input = sys.argv[1]
+sheet_name_input = sys.argv[2]
+data_profile_type_input = sys.argv[3]
+# excel_path_input = 'C:\\temp\\test_data_wkbk.xlsm'
+# sheet_name_input = 'TEST DATA'
+
+# %%
+# eda.excel_to_eda_tools(
+excel_to_eda_tools(
+    excel_path=excel_path_input,
+    sheet_name=sheet_name_input,
+    data_profile_type=data_profile_type_input,
+)
+
+# %%
