5abb4081090706565f946553b43c79d2a29bd367
fredapires <fredapires@gmail.com>
2023-04-01 00:21:02 -0400

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='fp-data-toolbox',
-    version='0.1.49',
+    version='0.1.50',
     author='Fred Pires',
     author_email='fredapires@gmail.com',
     description='personal toolbox for data science',
