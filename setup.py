cc720bd3ba474b45babe840c84de9b405e3d07de
fredapires <fredapires@gmail.com>
2023-04-07 12:27:04 -0400

    updating required python version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='fp-data-toolbox',
-    version='0.1.51',
+    version='0.1.52',
     author='Fred Pires',
     author_email='fredapires@gmail.com',
     description='personal toolbox for data science',
@@ -38,5 +38,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.9',
 )
