50dfaf0f2d9822de870f05109f5c4f7168e77b33
fredapires <fredapires@gmail.com>
2023-05-03 17:47:50 -0400

    update  test

--- scripts/batch/git-pre-push-hook-script.bat
+++ scripts/batch/git-pre-push-hook-script.bat
@@ -1,4 +1,7 @@
 @REM cd into package directory
 @echo off
-echo This is the git-pre-push-hook-script running.
 
+echo This is the git-pre-push-hook-script running.
+call .\scripts\batch\git-pre-push-hook-script.bat
+
+pause
---
+++ scripts/batch/project_push_sync/project_sync_to_onedrive.bat
@@ -0,0 +1,19 @@
+SET _SRC="C:\git\fp-data-toolbox-proj\"
+SET _DST="C:\Users\Fred\OneDrive\git_repos\fp-data-toolbox-proj\"
+
+:: add more folders to exclude here
+SET _xd=.git
+SET _xd=%_xd% .vscode
+SET _xd=%_xd% .venv
+SET _xd=%_xd% data
+SET _xd=%_xd% node_modules
+
+:: add more files to exclude here
+SET _xf=.env
+SET _xf=%_xf% .gitattributes
+
+:: set options
+SET _OPTION=/MIR /XD %_xd% /XF %_xf% 
+::/LOG:%_LOG%
+:: execute  robocopy
+ROBOCOPY %_SRC% %_DST% %_OPTION%
