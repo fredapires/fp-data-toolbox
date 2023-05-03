c86ae224fac8c83734c3939d168fb80def349af7
fredapires <fredapires@gmail.com>
2023-05-03 17:53:11 -0400

    update

--- scripts/batch/project_push_sync/project_sync_to_onedrive.bat
+++ scripts/batch/project_push_sync/project_sync_to_onedrive.bat
@@ -11,6 +11,7 @@ SET _xd=%_xd% node_modules
 :: add more files to exclude here
 SET _xf=.env
 SET _xf=%_xf% .gitattributes
+SET _xf=%_xf% poetry.lock
 
 :: set options
 SET _OPTION=/MIR /XD %_xd% /XF %_xf% 
