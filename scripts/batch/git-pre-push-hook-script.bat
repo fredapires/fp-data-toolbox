052de1bcedc5c5f76784890fe0a5eb8da38efb93
fredapires <fredapires@gmail.com>
2023-05-03 17:49:48 -0400

    update

--- scripts/batch/git-pre-push-hook-script.bat
+++ scripts/batch/git-pre-push-hook-script.bat
@@ -1,7 +1,7 @@
 @REM cd into package directory
 @echo off
 
-echo This is the git-pre-push-hook-script running.
+@REM echo This is the git-pre-push-hook-script running.
 call .\scripts\batch\git-pre-push-hook-script.bat
 
 pause
