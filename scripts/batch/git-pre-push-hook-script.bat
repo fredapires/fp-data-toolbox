656a8caba32aca3d9464bb512a9cab71bfc308f5
fredapires <fredapires@gmail.com>
2023-05-03 17:51:53 -0400

    update  test

--- scripts/batch/git-pre-push-hook-script.bat
+++ scripts/batch/git-pre-push-hook-script.bat
@@ -2,6 +2,6 @@
 @echo off
 
 @REM echo This is the git-pre-push-hook-script running.
-call .\scripts\batch\git-pre-push-hook-script.bat
+call .\scripts\batch\project_push_sync\project_sync_to_onedrive.bat
 
 pause
